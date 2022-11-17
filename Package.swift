3bfcf54fbf399f71decc227d124bfb03a5971de0
Mohamed Ghebaji <mohamed.ghebaji@radiofrance.com>
2022-11-17 13:01:42 +0100

    delete Quick

--- Package.swift
+++ Package.swift
@@ -1,21 +1,20 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "SpecLeaks",    
     platforms: [
-      .iOS(.v10)
+        .iOS(.v14)
     ],
     products: [        
         .library(name: "SpecLeaks", targets: ["SpecLeaks"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
-        .package(url: "https://github.com/Quick/Quick", from: "3.0.0"),
+        .package(url: "https://github.com/Quick/Nimble", exact: "10.0.0"),
     ],
     targets: [     
-        .target(name: "SpecLeaks", dependencies: ["Quick", "Nimble"], path: "SpecLeaks/Classes/"),
+        .target(name: "SpecLeaks", dependencies: ["Nimble"], path: "SpecLeaks/Classes/"),
     ],
     swiftLanguageVersions: [.v5]
 )
